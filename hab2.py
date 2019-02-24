79246ae18549aa967d4f68ecfd3f05278bb62cc8
Gunars Frickauss <gunarsf@gmail.com>
2019-02-24 11:36:02 +0200

    Add files via upload

--- hab2.py
+++ hab2.py
@@ -10,11 +10,16 @@ import struct
 
 COL ="""tim,dat,lat,lon,spd,dir,asl,siq,p_time,p_loss,
 current-operator,current-cellid,rsrq,rsrp,session-uptime,
-subscriber-number,sinr,cqi,lac,sector-id,earfcn,phy-cellid,enb-id,access-technology"""
+sinr,cqi,lac,sector-id,earfcn,phy-cellid,enb-id,access-technology"""
 
+PING_ADDRESS='8.8.8.8'
 MSG_INTERVAL=300 #Send SMS interval in sec
-GPS_INTERVAL=1 #GPS poll rate (sec)
-TEMP_INTERVAL=5 #DS18B20 poll rate (sec)
+MSG_PHONE='+37129413099' #Where to send status messages
+GPS_INTERVAL=1 #GPS refresh rate (sec)
+TEMP_INTERVAL=5 #DS18B20 refresh rate (sec)
+PING_INTERVAL=5 #Ping frequecy rate (sec)
+LOG_FILE_MASK="/home/pi/log/hab_*.csv"
+LOG_FILE_MAXLINES = 1000  # Max lines per log file
 
 
 class ds(object):
@@ -130,9 +135,13 @@ class logger(object):
         self._mask=mask
         self._file_name = self._mask.replace("*","{:04d}".format(len(glob.glob(self._mask))+1))
         self._header=header
-        if len(self._header)>0:
-                    with open(self._file_name, "a") as f:
-                        f.write(self._header+"\n")
+        self._running=True
+        try:
+            if len(self._header)>0:
+                with open(self._file_name, "a") as f:
+                    f.write(self._header+"\n")
+        except:
+		self._running=False
     def write(self,line):
         with open(self._file_name, "a") as f:
             f.write(line+"\n")
@@ -145,6 +154,8 @@ class logger(object):
                 if len(self._header)>0:
                     with open(self._file_name, "a") as f:
                         f.write(self._header+"\n")
+    def running(self):
+	return self._running
 
 class mt(object):
     def __init__(self, target='192.168.88.1',
@@ -178,39 +189,48 @@ class mt(object):
 	print("Sending to {}\n {}".format(phone,message))
 	stdin,stdout,stderr = self._ssh.exec_command('/')
 
-def ping(server="8.8.8.8", count=3, wait_sec=2):
-
-    """
-    :rtype: dict or None
-    """
-
-    cmd = "ping -c {} -W {} {}".format(count, wait_sec, server).split(' ')
-    try:
-        output = subprocess.check_output(cmd).decode().strip()
-        lines = output.split("\n")
-        total = lines[-2].split(',')[3].split()[1]
-        loss = lines[-2].split(',')[2].split()[0]
-        timing = lines[-1].split()[3].split('/')
-        return {
-            'p_server': server,
-            'p_min': timing[0],
-            'p_time': timing[1],
-            'p_max': timing[2],
-            'p_mdev': timing[3],
-            'p_total': total,
-            'p_loss': loss,
-        }
-    except Exception as e:
-        #print(e)
-        return {
-            'p_server': "N/A",
-            'p_min': "N/A",
-            'p_time': "N/A",
-            'p_max': "N/A",
-            'p_mdev': "N/A",
-            'p_total': "N/A",
-            'p_loss': "N/A",
-        }
+class pinger(object):
+    def __init__(self,server='8.8.8.8',count=3, wait_sec=2):
+        self._running=True
+        self.ping_ok=False
+        self._server=server
+        self._count=count
+        self._wait_sec=wait_sec
+    def running(self):
+        return(self._running)
+    def next(self):
+        try:
+            cmd = "ping -c {} -W {} {}".format(self._count, self._wait_sec, self._server).split(' ')
+            output = subprocess.check_output(cmd).decode().strip()
+        except Exception as e:
+            #print(e)
+            self.ping_ok=False
+            return {
+                'p_status': self.ping_ok,
+                'p_server': self._server,
+                'p_min': 'NaN',
+                'p_time': 'NaN',
+                'p_max': 'NaN',
+                'p_mdev': 'NaN',
+                'p_total': 'NaN',
+                'p_loss': 'NaN',
+            }
+        else:
+            self.ping_ok=True
+            lines = output.split("\n")
+            total = lines[-2].split(',')[3].split()[1]
+            loss = lines[-2].split(',')[2].split()[0]
+            timing = lines[-1].split()[3].split('/')
+            return{
+                'p_status': self.ping_ok,
+                'p_server': self._server,
+                'p_min': timing[0],
+                'p_time': timing[1],
+                'p_max': timing[2],
+                'p_mdev': timing[3],
+                'p_total': total,
+                'p_loss': loss,
+                }
 
 def merge_dicts(*dict_args):
     """
@@ -227,35 +247,66 @@ def merge_dicts(*dict_args):
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
+    try:
+        ipadr=socket.inet_ntoa(fcntl.ioctl(s.fileno(),
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', ifname[:15]))[20:24])
+    except Exception as e:
+        ipaddr=str(e)
+    return(ipadr)
 
 def main(col):
+    msg_templ="{} {} {}* "
     columns= col.split(",")
     m=mt(target='192.168.78.1')
-    log=logger(mask='/home/pi/log/hab_*.csv',maxlines=1000,header=col)
     next_msg=time.time()+MSG_INTERVAL
+    status_msg='IP: '+get_ip_address('eth0')
+    log=logger(mask=LOG_FILE_MASK,maxlines=LOG_FILE_MAXLINES,header=col)
+    status_msg=msg_templ.format(status_msg,'Data Log', log.running())
 
     #Poller for DS18B20 tempreature sensors
     tempr = Poller(ds(),TEMP_INTERVAL)
     tempr.daemon=True
-    tempr.start()
-
-    #Poller for GPS 
-    gpsp = Poller(usbgps(),GPS_INTERVAL)
+    try:
+        tempr.start()
+    except Exeption as e:
+        status_msg=msg_templ.format(status_msg,'Temperature error:', e)
+	pass
+    else:
+        status_msg=msg_templ.format(status_msg,'Temperature: ', tempr.session.running())
+	pass
+    #Poller for GPS dongle
+    gpsp = Poller(usbgps(),TEMP_INTERVAL)
     gpsp.daemon = True
-    gpsp.start()
+    try:
+        gpsp.start()
+    except Exeption as e:
+        status_msg=msg_templ.format(status_msg,'GPS error:', e)
+	pass
+    else:
+        status_msg=msg_templ.format(status_msg,'GPS: ', gpsp.session.running())
+	pass
+
+    #Poller for Ping
+    png = Poller(pinger(server=PING_ADDRESS), PING_INTERVAL)
+    png.daemon = True
+    try:
+        png.start()
+    except Exeption as e:
+        status_msg="{}\n{}".format(status_msg,'Ping error:', e)
+	pass
+    else:
+        status_msg=msg_templ.format(status_msg,'Ping: ', png.session.running())
+	pass
 
     #add temperature sensors to CSC
     columns=columns+tempr.session.deviceIds()
+    m.send_sms(phone=MSG_PHONE, message=status_msg)
 
-    m.send_sms(message=get_ip_address('eth0'))
-
-    print (columns)
+    #print (columns)
     while True:
         r=""
-        l=merge_dicts(m.read_all(), ping(server='8.8.8.8'), gpsp.get_current_value(), tempr.get_current_value())
+        l=merge_dicts(m.read_all(), png.get_current_value(), gpsp.get_current_value(), tempr.get_current_value())
         for i in columns:
             try:
                 r=r+(l[i].replace(",",";"))+","
@@ -263,10 +314,11 @@ def main(col):
 		print(e)
                 r=r+"N/A,"
         print(r)
-        log.write(r)
+        
+	log.write(r)
         if time.time()>next_msg:
             msg='http://maps.google.com?q={},{} asl:{} spd:{}'.format(l['lat'],l['lon'],l['asl'],l['spd'])
-	    m.send_sms(message=msg)
+	    m.send_sms(phone=MSG_PHONE, message=msg)
             next_msg=time.time()+MSG_INTERVAL
         time.sleep(5)
     #for k,v in l.items():
